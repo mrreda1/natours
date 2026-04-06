a3471691bbf64849c8b7b5b2ce5960c4aefd5f7f
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-06 05:57:09 +0200

    Adding reviews to tours with the tour id parameter

--- controllers/review.js
+++ controllers/review.js
@@ -36,6 +36,13 @@ exports.getReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  if(!req.body.tour) {
+    req.body.tour = req.params.tourID;
+  }
+  if(!req.body.user) {
+    req.body.user = req.user.id;
+  }
+
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: "success",
--- routes/tours.js
+++ routes/tours.js
@@ -3,34 +3,43 @@ const express = require("express");
 const router = express.Router();
 const controller = require(`${__dirname}/../controllers/tours`);
 const authController = require("./../controllers/auth");
+const reviewController = require("./../controllers/review");
 
 router
-	.route("/top-5-cheap")
-	.get(
-		authController.protect,
-		controller.aliasTopTours,
-		controller.getAllTours,
-	);
+  .route("/top-5-cheap")
+  .get(
+    authController.protect,
+    controller.aliasTopTours,
+    controller.getAllTours,
+  );
 
 router
-	.route("/")
-	.get(authController.protect, controller.getAllTours)
-	.post(authController.protect, controller.createTour);
+  .route("/")
+  .get(authController.protect, controller.getAllTours)
+  .post(authController.protect, controller.createTour);
 
 router.route("/stats").get(authController.protect, controller.getTourStats);
 
 router
-	.route("/monthly-plan/:year")
-	.get(authController.protect, controller.getMonthlyPlan);
+  .route("/monthly-plan/:year")
+  .get(authController.protect, controller.getMonthlyPlan);
 
 router
-	.route("/:id")
-	.get(controller.getTour)
-	.patch(controller.updateTour)
-	.delete(
-		authController.protect,
-		authController.restrictTo("admin", "lead-guide"),
-		controller.deleteTour,
-	);
+  .route("/:id")
+  .get(controller.getTour)
+  .patch(controller.updateTour)
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin", "lead-guide"),
+    controller.deleteTour,
+  );
+
+router
+  .route("/:tourID/reviews")
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview,
+  );
 
 module.exports = router;
